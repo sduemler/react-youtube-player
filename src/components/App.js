78f88fc99f7fb9c4042e4574d6925945a68777f7
Sam Duemler <samduemler@gmail.com>
2019-08-06 22:05:07 -0500

    Addded shown description when clicking on search results.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async (term) => {
         const response = await youtube.get(
@@ -13,15 +14,19 @@ class App extends React.Component {
                 q: term
             }
         });
-
         this.setState({ videos: response.data.items });
+    };
+
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video})
     };
 
     render() {
         return (
           <div className="ui container">
               <SearchBar onTermSubmit={this.onTermSubmit} />
-              <VideoList videos={this.state.videos}/>
+              <VideoDetail video={this.state.selectedVideo} />
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
           </div>
         );
     }
